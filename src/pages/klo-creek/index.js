4795b3c223f706f65a919dcc8b981e4222400172
Dane Ashman <daneashman@Danes-Air.lan>
2024-01-12 14:30:16 -0800

    commit

--- src/pages/klo-creek/index.js
+++ src/pages/klo-creek/index.js
@@ -1,12 +1,39 @@
 import * as React from "react";
 import Layout from "../../components/Layout";
+import { graphql } from "gatsby";
 
-const KloCreek = () => {
+function KloCreek({ data }) {
+  console.log(data);
   return (
     <Layout>
       <h1>Welcome to KLO Creek!</h1>
+      {data.allMarkdownRemark.group.map((i) => (
+        <li key={i.group[0].nodes[0].fields.slug}>
+          {i.group[0].nodes[0].frontmatter.name}
+        </li>
+      ))}
     </Layout>
   );
-};
+}
+
+export const kloCreekQuery = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      group(field: frontmatter___wall) {
+        group(field: id) {
+          nodes {
+            fields {
+              slug
+            }
+            frontmatter {
+              name
+              wall
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default KloCreek;
