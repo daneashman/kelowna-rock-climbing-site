923172e41fe73dc523e294e72450f29eeefd3a58
Dane Ashman <daneashman@Danes-Air.lan>
2024-01-12 13:11:33 -0800

    created KLO creek collection

---
+++ src/pages/klo-creek/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../../components/Layout";
+
+const KloCreek = () => {
+  return (
+    <Layout>
+      <h1>Welcome to KLO Creek!</h1>
+    </Layout>
+  );
+};
+
+export default KloCreek;
---
+++ src/templates/climb.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+export default function climb() {
+  return (
+    <Layout>
+      <h1>I am a Climb!</h1>
+    </Layout>
+  );
+}
