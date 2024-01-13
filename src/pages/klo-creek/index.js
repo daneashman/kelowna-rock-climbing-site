eb95b6ae1e2e3f89e96ff38f000a6066533501c6
Dane Ashman <daneashman@outlook.com>
2024-01-12 22:59:49 -0800

    trying nesting

--- src/pages/klo-creek/index.js
+++ src/pages/klo-creek/index.js
@@ -9,33 +9,33 @@ function KloCreek({ data }) {
       <h1>Welcome to KLO Creek!</h1>
       <h2>Areas</h2>
       <h2>Climbs</h2>
-      {data.allMarkdownRemark.group.map((i) => (
+      {/* {data.allMarkdownRemark.group.map((i) => (
         <li key={i.group[0].nodes[0].fields.slug}>
           {i.group[0].nodes[0].frontmatter.title}
         </li>
-      ))}
+      ))} */}
     </Layout>
   );
 }
 
-export const kloCreekQuery = graphql`
-  query MyQuery {
-    allMarkdownRemark {
-      group(field: frontmatter___wall) {
-        group(field: id) {
-          nodes {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              wall
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+// export const kloCreekQuery = graphql`
+//   query MyQuery {
+//     allMarkdownRemark {
+//       group(field: frontmatter___wall) {
+//         group(field: id) {
+//           nodes {
+//             fields {
+//               slug
+//             }
+//             frontmatter {
+//               title
+//               wall
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
 
 export default KloCreek;
